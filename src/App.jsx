f84229bb904cfc09786f8915991d28381b029eea
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-03-30 20:04:55 +0100

    Update the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,99 @@
+import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { FaArrowUp } from "react-icons/fa";
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
     <>
       <Header />
       <main>
-        <h1>Main here</h1>
+        <div
+          id="about"
+          style={{
+            height: "90vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            color: "white",
+            background: "black",
+          }}
+        >
+          about
+        </div>
+
+        <div
+          id="experience"
+          style={{
+            height: "80vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            color: "white",
+            background: "red",
+          }}
+        >
+          Experience
+        </div>
+
+        <div
+          id="work"
+          style={{
+            height: "80vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            color: "white",
+            background: "yellow",
+          }}
+        >
+          Experience
+        </div>
+
+        <div
+          id="contact"
+          style={{
+            height: "80vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            color: "black",
+            background: "white",
+          }}
+        >
+          Experience
+        </div>
+        {isVisible && (
+          <button className="back-top" type="button" onClick={scrollToTop}>
+            <FaArrowUp />
+          </button>
+        )}
       </main>
 
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,67 +3,84 @@ import useWindowWidth from "@/hooks/useWindowWidth";
 import Resume from "@/data/resume.pdf";
 import ButtonLink from "./ButtonLink";
 import MenuImg from "@/assets/img/menu.svg";
+import CloseImg from "@/assets/img/close.svg";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   const width = useWindowWidth();
 
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleScrollClick = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({ behavior: "smooth" });
+    setShowMenu(!showMenu);
+  };
+
   useEffect(() => {
-    if (width < 768) {
-      setShowMenu(true);
-      console.log(width);
-    } else {
-      setShowMenu(false);
-      console.log(width);
-    }
-    return () => {};
-  }, [showMenu, width]);
+    setShowMenu(false);
+  }, [width]);
+
   return (
     <header className="header">
-      {!showMenu && (
-        <div className="header__wrapper">
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item">
-                <a href="#about">
-                  <span>01.</span>
-                  <span>About</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a href="#experience">
-                  <span>02.</span>
-                  <span>Experience</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a href="#work">
-                  <span>03.</span>
-                  <span>work</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a href="#contact">
-                  <span>04.</span>
-                  <span>Contact</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <ButtonLink
-            href={Resume}
-            download="Animashaun Fisayo Resume.pdf"
-            className="header__download"
-          >
-            Resume
-          </ButtonLink>
-        </div>
-      )}
-      {showMenu && (
-        <button className="header__menu-btn">
-          <img src={MenuImg} alt="mobile menu button" />
-        </button>
+      <div
+        className={`header__wrapper ${showMenu && width < 769 ? "open" : ""}`}
+      >
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <a href="#about" onClick={handleScrollClick}>
+                <span>01.</span>
+                <span>About</span>
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a href="#experience" onClick={handleScrollClick}>
+                <span>02.</span>
+                <span>Experience</span>
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a href="#work" onClick={handleScrollClick}>
+                <span>03.</span>
+                <span>work</span>
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a href="#contact" onClick={handleScrollClick}>
+                <span>04.</span>
+                <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <ButtonLink
+          href={Resume}
+          download="Animashaun Fisayo Resume.pdf"
+          className="header__download"
+        >
+          Resume
+        </ButtonLink>
+      </div>
+      {width < 769 && (
+        <>
+          {!showMenu && (
+            <button className="header__menu-btn" onClick={handleMenu}>
+              <img src={MenuImg} alt="mobile menu button" />
+            </button>
+          )}
+
+          {showMenu && (
+            <button className="header__menu-btn open" onClick={handleMenu}>
+              <img src={CloseImg} alt="mobile menu button" />
+            </button>
+          )}
+        </>
       )}
     </header>
   );
