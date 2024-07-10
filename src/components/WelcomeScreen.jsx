89d86f4b0b9e8c817d1671633512a8d43a4a79a1
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-10 19:53:10 +0000

    Update Welcome screen letter

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -7,7 +7,7 @@ const WelcomeScreen = () => {
           d="M50 10 L10 30 L10 70 L50 90 L90 70 L90 30 Z"
         />
         <text x="50%" y="50%" textAnchor="middle" dy=".3em" className="letter">
-          B
+          F
         </text>
       </svg>
     </div>
