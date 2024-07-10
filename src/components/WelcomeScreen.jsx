deccc9cd0e3638888fe67f1ab4f4cd80455e8e42
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-10 19:52:38 +0000

    Update welcome Screen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,7 +1,15 @@
 const WelcomeScreen = () => {
   return (
     <div className="welcome">
-      <div className="welcome__content"></div>
+      <svg className="hexagon" viewBox="0 0 100 100">
+        <path
+          id="hexagon-path"
+          d="M50 10 L10 30 L10 70 L50 90 L90 70 L90 30 Z"
+        />
+        <text x="50%" y="50%" textAnchor="middle" dy=".3em" className="letter">
+          B
+        </text>
+      </svg>
     </div>
   );
 };
