3311dc1045f1bc64446fb0f90f399bc99ee981d1
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-18 17:46:06 +0100

    Update the job description

--- src/components/ExperienceContent.jsx
+++ src/components/ExperienceContent.jsx
@@ -1,9 +1,19 @@
-import { ExperienceContentHeader } from "./ExperienceContentHeader";
+import { useState } from "react";
+import { authorData } from "@/data/authorData";
+import ExperienceContentHeader from "./ExperienceContentHeader";
 
 const ExperienceContent = () => {
+  const [tabIndex, setTabIndex] = useState(1);
+  const experiences = authorData.experience;
+
+  const currentExperience = experiences.find((exp) => exp.id === tabIndex);
+
   return (
     <div className="experience__wrapper">
-      <ExperienceContentHeader />
+      <ExperienceContentHeader experienceData={experiences} index={tabIndex} setTabIndex={setTabIndex}/>
+      <div className="experience__content">
+        <h3 className="experience__subheading">{currentExperience.position}</h3>
+      </div>
     </div>
   );
 };
--- src/components/ExperienceContentHeader.jsx
+++ src/components/ExperienceContentHeader.jsx
@@ -1,21 +1,25 @@
-import { authorData } from "@/data/authorData";
-import { useState } from "react";
+import PropTypes from "prop-types";
+
 import useContainerHeight from "@/hooks/useContainerHeight";
 import Marker from "./Marker";
 
-export const ExperienceContentHeader = () => {
-  const [tabIndex, setTabIndex] = useState(1);
-	const [containerRef, height] = useContainerHeight();
-  const experiences = authorData.experience;
+const ExperienceContentHeader = ({ experienceData, index, setTabIndex }) => {
+  const [containerRef, height] = useContainerHeight();
 
   return (
     <div ref={containerRef} className="experience__tabs">
-			<Marker className="experience__marker" height={height / experiences.length} offset={(tabIndex - 1) * (height / experiences.length)} />
-			
-      {experiences.map((data) => (
+      <Marker
+        className="experience__marker"
+        height={height / experienceData.length}
+        offset={(index - 1) * (height / experienceData.length)}
+      />
+
+      {experienceData.map((data) => (
         <button
           key={data.id}
-          className={`experience__tab-item ${tabIndex === data.id ? "active" : ""}`}
+          className={`experience__tab-item ${
+            index === data.id ? "active" : ""
+          }`}
           onClick={() => setTabIndex(data.id)}
         >
           {data.company}
@@ -24,3 +28,20 @@ export const ExperienceContentHeader = () => {
     </div>
   );
 };
+
+ExperienceContentHeader.propTypes = {
+  index: PropTypes.number.isRequired,
+  setTabIndex: PropTypes.func.isRequired,
+  experienceData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      company: PropTypes.string.isRequired,
+      position: PropTypes.string.isRequired,
+      startDate: PropTypes.string.isRequired,
+      endDate: PropTypes.string.isRequired,
+      description: PropTypes.arrayOf(PropTypes.string).isRequired,
+    })
+  ).isRequired,
+};
+
+export default ExperienceContentHeader;
