fd1716e42cc21d318f2ac29e8de0b2ab72466d12
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-18 01:21:23 +0100

    Update about

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -9,18 +9,32 @@ const AboutContent = () => {
           developer. I build scalable, user-centric solutions that blend
           engineering precision with cloud innovation. My journey into
           technology began in earnest in 2022 when I led FM Technologies
-          deployment at Max-Migold. Starting with HTML, CSS, and JavaScript for
-          a simple facilities management dashboard, I progressed to deploying an
-          open-source Moodle LMS on shared hosting, which I&apos;ve since
-          migrated to AWS as my cloud engineering skills advanced.
+          deployment at{" "}
+          <a href="#" target="_blank">
+            Max-Migold
+          </a>
+          . Starting with HTML, CSS, and JavaScript for a simple facilities
+          management dashboard, I progressed to deploying an open-source Moodle
+          LMS on shared hosting, which I&apos;ve since migrated to AWS as my
+          cloud engineering skills advanced.
         </p>
         <p>
-          Today, I&apos;m driving internal software development at Max-Migold
-          and Star University, while also building JoyMatcher, a professional
-          matchmaking platform. My focus has expanded beyond facilities
-          management to encompass the full software engineering lifecycle,
-          leveraging DevOps practices to create robust, efficient solutions
-          across various domains.
+          Today, I&apos;m driving internal software development at{" "}
+          <a href="#" target="_blank">
+            Max-Migold
+          </a>{" "}
+          and{" "}
+          <a href="#" target="_blank">
+            Star University
+          </a>
+          , while also building{" "}
+          <a href="#" target="_blank">
+            JoyMatcher
+          </a>
+          , a professional matchmaking platform. My focus has expanded beyond
+          facilities management to encompass the full software engineering
+          lifecycle, leveraging DevOps practices to create robust, efficient
+          solutions across various domains.
         </p>
         <p>
           This diverse experience allows me to approach problems with a unique
@@ -30,35 +44,35 @@ const AboutContent = () => {
         </p>
         <p>Here are a few technologies I’ve been working with recently:</p>
         <div className="about__tech">
-        <ul>
-  <li>JavaScript (ES6)</li>
-  <li>React</li>
-  <li>React Bootstrap</li>
-  <li>Redux Toolkit</li>
-  <li>React Router</li>
-  <li>React Testing Library</li>
-  <li>Node (Express.js)</li>
-</ul>
+          <ul>
+            <li>JavaScript (ES6)</li>
+            <li>React</li>
+            <li>React Bootstrap</li>
+            <li>Redux Toolkit</li>
+            <li>React Router</li>
+            <li>React Testing Library</li>
+            <li>Node (Express.js)</li>
+          </ul>
 
-<ul>
-  <li>Ruby on Rails</li>
-  <li>RSpec</li>
-  <li>SASS</li>
-  <li>Tailwind CSS</li>
-  <li>Webpack/Vite</li>
-  <li>Jest</li>
-  <li>ESLint</li>
-</ul>
+          <ul>
+            <li>Ruby on Rails</li>
+            <li>RSpec</li>
+            <li>SASS</li>
+            <li>Tailwind CSS</li>
+            <li>Webpack/Vite</li>
+            <li>Jest</li>
+            <li>ESLint</li>
+          </ul>
 
-<ul>
-  <li>Rubocop</li>
-  <li>Stylelint</li>
-  <li>Git/GitHub</li>
-  <li>PostgreSQL</li>
-  <li>MongoDB</li>
-  <li>AWS/GCP</li>
-  <li>Figma</li>
-</ul>
+          <ul>
+            <li>Rubocop</li>
+            <li>Stylelint</li>
+            <li>Git/GitHub</li>
+            <li>PostgreSQL</li>
+            <li>MongoDB</li>
+            <li>AWS/GCP</li>
+            <li>Figma</li>
+          </ul>
         </div>
       </div>
       <div className="about__wrapper-right">
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,14 +7,26 @@ const Intro = () => {
       <p className="intro__greeting">Hi, My Name is</p>
       <h2 className="intro__name">Engr. Animashaun F.M</h2>
       <p className="intro__tagline">
-        Full-stack Developer | React, Javascript & Typescript, Ruby, PHP, Node, Rails |
-        Moodle LMS Expert | Aspiring DevOps Engineer | MNSE, COREN, FM, CFM Certified
+        Full-stack Developer | React, Javascript & Typescript, Ruby, PHP, Node,
+        Rails | Moodle LMS Expert | Aspiring DevOps Engineer | MNSE, COREN, FM,
+        CFM Certified
       </p>
       <p className="intro__description">
         I&apos;m a full-stack developer specializing in web, mobile and cloud
-        solutions. Currently, I&apos;m focused on internal software development at{" "}
-        <a href="#" target="_blank">Max-Migold</a> and <a href="#" target="_blank">Star University</a>, and building <a href="#" target="_blank">JoyMatcher</a>, a professional
-        matchmaking platform.
+        solutions. Currently, I&apos;m focused on internal software development
+        at{" "}
+        <a href="#" target="_blank">
+          Max-Migold
+        </a>{" "}
+        and{" "}
+        <a href="#" target="_blank">
+          Star University
+        </a>
+        , and building{" "}
+        <a href="#" target="_blank">
+          JoyMatcher
+        </a>
+        , a professional matchmaking platform.
       </p>
       <ButtonLink href={Resume} className="intro__download">
         Resume
