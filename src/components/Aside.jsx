4b413c69e6f01df6fd0170eb5e51272a93705faf
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-21 02:24:59 +0100

    Add Section transtion and header entering animation

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 const Aside = ({ children, className }) => {
-  return <aside className={`aside ${className}`}>{children}</aside>;
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const currentSection = sectionRef.current;
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    if (currentSection) {
+      observer.observe(currentSection);
+    }
+
+    return () => {
+      if (currentSection) {
+        observer.unobserve(currentSection);
+      }
+    };
+  }, []);
+  return <aside ref={sectionRef} className={`aside ${className}`}>{children}</aside>;
 };
 
 Aside.propTypes = {
