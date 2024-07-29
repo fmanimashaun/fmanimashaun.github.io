f9267cd91bb4e3f257d5fe4e4f5cabb4ca12bacf
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-29 10:02:28 +0100

    Update author data

--- src/data/authorData.js
+++ src/data/authorData.js
@@ -6,11 +6,17 @@ export const authorData = {
       position: "Technology Advisor",
       startDate: "Jan. 2022",
       endDate: "Present",
-      description: [
-        "Lead a team of 3 software engineers and 3 product designers in developing internal software solutions and client projects.",
-        "Spearhead both frontend and backend development, ensuring high-quality, responsive, and user-friendly interfaces.",
-        "Manage the entire software development lifecycle, from conception to deployment.",
-        "Oversee the development and enhancement of the company's Learning Management System (LMS).",
+      description:[
+        "Led the entire full-stack development for internal software solutions, with a primary focus on frontend technologies",
+        "Spearheaded frontend development using React, JavaScript, and SASS/SCSS, creating responsive and intuitive user interfaces",
+        "Deployed and managed an open-source Moodle LMS on AWS, optimizing performance and load times by leveraging cloud scalability",
+        "Implemented responsive design principles and modern SASS techniques to ensure cross-device compatibility",
+        "Integrated RESTful APIs and implemented state management using Redux for efficient data flow",
+        "Utilized AWS services (e.g., EC2, S3, RDS) to ensure optimal performance and scalability of web applications",
+        "Collaborated with UX/UI designers to improve user experience and implement design systems",
+        "Contributed to backend development using Node.js and Ruby on Rails, gaining full-stack proficiency",
+        "Implemented GitHub Actions for enforcing coding styles and automated testing, ensuring high code quality",
+        "Conducted code reviews and mentored junior developers, promoting best practices and code quality"
       ],
     },
     {
@@ -20,10 +26,13 @@ export const authorData = {
       startDate: "Apr, 2024",
       endDate: "Present",
       description: [
-        "Lead the development of a professional matchmaking platform, utilizing Rails for backend and React for frontend.",
-        "Implement and manage CI/CD pipelines using GitHub Actions, ensuring smooth deployment and integration processes.",
-        "Collaborate with UX/UI designers to create an intuitive and responsive user interface.",
-        "Implement robust security measures to protect user data and ensure platform integrity.",
+        "Developing a professional matchmaking platform, implementing both frontend (React, JavaScript) and backend (Ruby on Rails) solutions",
+        "Utilizing Redux Toolkit for state management and Redux Toolkit Query for efficient API integration, ensuring optimal frontend performance and data flow",
+        "Implementing responsive UI components with SASS, while using PropTypes for type checking to enhance code reliability",
+        "Designing RESTful APIs and managing PostgreSQL database for robust backend functionality",
+        "Implementing user authentication, authorization, error handling, and logging across the full stack",
+        "Utilizing GitHub Actions for automated testing and code quality checks, maintaining high development standards",
+        "Collaborating with stakeholders to refine features and plan future enhancements, including TypeScript migration post-MVP"
       ],
     },
 
@@ -34,11 +43,13 @@ export const authorData = {
       startDate: "Sept, 2022",
       endDate: "Feb, 2024",
       description: [
-        "Completed an intensive, full-time full-stack engineering program, dedicating over 1,500 hours to mastering both front-end and back-end technologies",
-        "Developed and deployed 20+ web applications using React, Ruby on Rails, JavaScript, HTML/CSS, and SQL",
-        "Collaborated with international peers in pair programming sessions, simulating real-world remote work environments",
-        "Managed complex projects using agile methodologies, including daily stand-ups, sprint planning, and code reviews",
-        "Mentored junior trainees, improving leadership and communication skills",
+        "Completed an intensive full-stack engineering program, focusing on both frontend and backend technologies",
+        "Developed and deployed 15+ web applications using React, JavaScript, HTML5, and Sass",
+        "Gained proficiency in backend development with Ruby on Rails and database management using PostgreSQL",
+        "Collaborated in pair programming sessions, simulating real-world remote work environments",
+        "Implemented responsive designs and ensured cross-browser compatibility for various projects",
+        "Utilized version control with Git and GitHub, participating in code reviews and maintaining collaborative codebases",
+        "Practiced agile methodologies, including daily stand-ups, sprint planning, and project management with Kanban boards"
       ],
     },
   ],
